65ccb6e093a394da5af11f04161ae51edc9989dc
fightingdogeza <waruka698@gmail.com>
2025-12-10 09:59:10 +0900

    境界線の長さ弄り

--- project/js/dashboard.js
+++ project/js/dashboard.js
@@ -164,7 +164,7 @@ function renderPins(pins) {
       ${pin.image_path ? `<img src="${pin.image_path}"/>` : ""}<br>
       <button class="goto-map-btn">地図で見る</button>
       <button class="delete-btn">削除</button><br>
-      <p><strong>ーーーーーーーーーーーーーーーーーーーーーーーー</strong></p>
+      <p><strong>---------------</strong></p>
     `;
     card.querySelector(".delete-btn").addEventListener("click", () => {
       card.querySelector(".delete-btn").disabled = "false";
